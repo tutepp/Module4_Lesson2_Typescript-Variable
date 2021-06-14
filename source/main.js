e204fab9d35013a084107f324b3148cc341dcacf
Your Name <hoangtu1297hp@gmail.com>
2021-06-14 09:26:24 +0700

    TS-Variable

---
+++ source/main.js
@@ -0,0 +1,6 @@
+var width;
+var heigth;
+width = 10.5;
+heigth = 20;
+var area = width * heigth;
+console.log("Dien tich hinh chu nhat: " + area);
---
+++ source/main.ts
@@ -0,0 +1,8 @@
+let width: number;
+let heigth: number;
+
+width = 10.5;
+heigth = 20;
+
+let area: number = width*heigth;
+console.log(`Dien tich hinh chu nhat: ${area}`);
